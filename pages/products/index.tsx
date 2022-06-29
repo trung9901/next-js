a52bfc9d325f0bd4c4f9ddf70fae42062553a45c
Trung <trungnhph13487@fpt.edu.vn>
2022-06-29 22:16:08 +0700

    a

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,39 +1,48 @@
 import Link from 'next/link';
 import { GetStaticProps, GetStaticPropsContext } from 'next';
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 
 type Props = {
-    products: any[];
-}
+  products: any[];
+};
 
-const ProductsList = ({products}: Props) => {
+const ProductsList = ({ products }: Props) => {
   return (
     <div>
-        {products.map((item) =>(
-            <div key={item.id}>
-               
-                <Link href={`/products/${item.id}`} className="">{item.name}</Link>
-                
-            </div>
-            
-        )) }
+      {products.map((item) => (
+        <div key={item.id}>
+          <Link href={`/products/${item.id}`} className="">
+            {item.name}
+          </Link>
+          <Image
+            src="me.png"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+        </div>
+      ))}
     </div>
-  )
-}
-export const getStaticProps: GetStaticProps<Props> = async (context: GetStaticPropsContext) => {
-    const res = await fetch('https://620237e9b8735d00174cb87f.mockapi.io/products')
-    const data = await res.json()
-    console.log('data', data);
-    if(!data){
-        return { 
-            notFound: true
-        }
-    }
+  );
+};
+export const getStaticProps: GetStaticProps<Props> = async (
+  context: GetStaticPropsContext
+) => {
+  const res = await fetch(
+    'https://620237e9b8735d00174cb87f.mockapi.io/products'
+  );
+  const data = await res.json();
+  console.log('data', data);
+  if (!data) {
     return {
-      props: {
-        products: data
-      }, 
-    }
+      notFound: true,
+    };
   }
-export default ProductsList
+  return {
+    props: {
+      products: data,
+    },
+  };
+};
+export default ProductsList;
