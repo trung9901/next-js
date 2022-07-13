145bb681898e74729d23e590502dfc72386f1a97
Trung <trungnhph13487@fpt.edu.vn>
2022-07-13 15:26:58 +0700

    a

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -4,18 +4,20 @@ import Layout from './../../components/Layout/index';
 import LayoutAdmin from './../../components/Layout/admin';
 
 type Props = {};
-// const useUser = () => ({ user: 1 })
 
 const admin = (props: Props) => {
-  // const { user } = useUser()
-  // const router = useRouter()
-  // useEffect(() => {
-  //     if (user === 1) {
-  //         console.log('admin')
-  //         router.push('/')
-  //         alert('abc')
-  //     }
-  // }, [])
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const user = 1;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    if (user === 1) {
+      console.log('ban ko phai admin');
+      router.push('/');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <div>admin</div>;
 };
 admin.Layout = LayoutAdmin;
