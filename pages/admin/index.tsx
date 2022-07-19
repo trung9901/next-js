82b6151707b191a54ee4799b1e0e737da9cfb9ac
Trung <trungnhph13487@fpt.edu.vn>
2022-07-19 11:34:13 +0700

    a

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -12,10 +12,10 @@ const admin = (props: Props) => {
   const router = useRouter();
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    if (user === 1) {
-      console.log('ban ko phai admin');
-      router.push('/');
-    }
+    // if (user === 1) {
+    //   console.log('ban ko phai admin');
+    //   router.push('/');
+    // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div>admin</div>;
